8a307597071e64d6b4ac69a5058377ee9b5c5b34
salaman <sk2562446@gmail.com>
2025-02-03 02:05:50 +0530

    netlify cors

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "https://inquisitive-kangaroo-5884ee.netlify.app/",
+      "http://localhost:3000",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
